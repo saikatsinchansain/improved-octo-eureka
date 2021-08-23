4bea710c7f320503f81f825b9c7fff4e9d8bc32c
saikatsain <saikatsinchansain@gmail.com>
2021-08-23 16:12:19 +0530

    Added logging!

--- api/main.py
+++ api/main.py
@@ -1,13 +1,19 @@
 from fastapi import Depends, FastAPI
 #from .dependencies import get_query_token, get_token_header
-
 from routers import profiles
+from base_logger import logger
+
+logger.info("**************** Starting application server!!! ***************")
 
 #app = FastAPI(dependencies=[Depends(get_query_token)])
 app = FastAPI()
+
+
 
 app.include_router(profiles.router)
 
 @app.get("/")
 async def root():
     return {"message": "Hello Bigger Applications!"}
+
+logger.info("*********** Application server initialization completed!!! ***************")
--- api/routers/profiles.py
+++ api/routers/profiles.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from models import models,tables
 from sql import database
+from base_logger import logger
+
 
 router = APIRouter(
     prefix="/profiles",
@@ -11,14 +13,17 @@ router = APIRouter(
 #A route to return all of the available entries for profile.
 @router.get('/all', summary='Get list of all profiles')
 async def api_all_profiles():
+    logger.info("Invoking api_all_profiles")
     return tables.get_all_profile(database.Session())
 
 #A route to return matching entries for profile.
 @router.get('/{uuid}',summary='Get profile')
 async def api_profile_by_uuid(uuid):
+    logger.info("Invoking api_profile_by_uuid with uuid:{}".format(uuid))
     return tables.get_profile(database.Session(),uuid)
 
 #A route to add new profile.
 @router.post('/', summary='Insert new profile')
 async def insert_profile(profile: models.ProfileVO):
+    logger.info("Invoking insert_profile with request:{}".format(profile))
     return tables.create_profile(database.Session(),profile)
